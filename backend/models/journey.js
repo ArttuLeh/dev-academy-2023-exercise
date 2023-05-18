a8765fc5b1f2f48014246a392ce87baade497bef
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-18 14:30:13 +0300

    updated journey model

--- backend/models/journey.js
+++ backend/models/journey.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose')
 
 const journeySchema = new mongoose.Schema({
   Departure_station_id: Number,
+  Departure_station_name: String,
   Return_station_id: Number,
+  Return_station_name: String,
+  Covered_distance_m: Number,
+  Duration_sec: Number,
 })
 
 journeySchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
+    delete returnedObject.__v
   },
 })
 module.exports = mongoose.model('Journey', journeySchema)
