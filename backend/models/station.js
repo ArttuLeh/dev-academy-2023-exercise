937379d445049d36953d1a2ea263a134dfcc9663
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-18 14:31:00 +0300

    updated station model

---
+++ backend/models/station.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const stationsSchema = new mongoose.Schema({
+  FID: Number,
+  ID: Number,
+  Nimi: String,
+  Namn: String,
+  Name: String,
+  Osoite: String,
+  Adress: String,
+  Kaupunki: String,
+  Stad: String,
+  Operaattor: String,
+  Kapasiteet: Number,
+  x: Number,
+  y: Number,
+})
+
+stationsSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+module.exports = mongoose.model('Station', stationsSchema)
