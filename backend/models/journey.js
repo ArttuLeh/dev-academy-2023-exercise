2189031609e37a3ee6ebbe0ab37b125c866e0de0
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-19 12:05:35 +0300

    some comments removed

--- backend/models/journey.js
+++ backend/models/journey.js
@@ -8,12 +8,4 @@ const journeySchema = new mongoose.Schema({
   Covered_distance_m: Number,
   Duration_sec: Number,
 })
-
-journeySchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
 module.exports = mongoose.model('Journey', journeySchema)
