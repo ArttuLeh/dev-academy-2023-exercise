7824d3e0862f453b1147a235ae3e33d26e98ceff
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-27 22:52:33 +0300

    added the search and sorting values

--- frontend/src/services/journeys.js
+++ frontend/src/services/journeys.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 const baseUrl = '/api/journeys'
 
-const getAll = async (currentPage) => {
-  const request = await axios.get(`${baseUrl}?page=${currentPage}`)
+//axios fetch the data from backend
+const getAll = async (currentPage, sortField, sortOrder, searchTerm) => {
+  const request = await axios.get(
+    `${baseUrl}?page=${currentPage}&sortField=${sortField}&sortOrder=${sortOrder}&search=${searchTerm}`
+  )
   return request.data
 }
 // eslint-disable-next-line
