90bfcd91647fe2878af4272c795810f32a373ba1
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-27 23:02:57 +0300

    comments added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,16 @@ import stationsReducer from './reducers/stationsReducer'
 import stationReducer from './reducers/stationReducer'
 import journeyReducer from './reducers/journeyReducer'
 import loadingReducer from './reducers/loadingReducer'
+import notificationReducer from './reducers/notificationReducer'
 
+// store the all states
 const store = configureStore({
   reducer: {
     stations: stationsReducer,
     station: stationReducer,
     journeys: journeyReducer,
     loading: loadingReducer,
+    notification: notificationReducer,
   },
 })
 export default store
