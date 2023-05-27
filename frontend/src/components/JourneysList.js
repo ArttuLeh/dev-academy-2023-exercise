bff5449068b9efafdb1f148261a53e9472feed14
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-27 23:02:22 +0300

    changed component to control the displayed data

--- frontend/src/components/JourneysList.js
+++ frontend/src/components/JourneysList.js
@@ -1,91 +1,101 @@
 import { useDispatch, useSelector } from 'react-redux'
-import {
-  Paper,
-  TableContainer,
-  Table,
-  TableBody,
-  TableRow,
-  TableCell,
-  TableHead,
-  Stack,
-  Pagination,
-  Box,
-  CircularProgress,
-} from '@mui/material'
+import { Box, Button, TextField } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { initializeJourneys } from '../reducers/journeyReducer'
+import SearchedJourneysData from './SearchedJourneysData'
+import JourneysData from './JourneysData'
 
+// component that handle which jorneys show, searched or all
 const JourneysList = () => {
   const dispatch = useDispatch()
-  const [currentPage, setCurrentPage] = useState(1)
-  const journeys = useSelector(({ journeys }) => journeys)
+  const [currentPage, setCurrentPage] = useState(1) // state for curren page
+  const [sortField, setSortField] = useState('') // state for sortfield
+  const [sortOrder, setSortOrder] = useState('') // state for sortorder (asc or desc)
+  const [searchTerm, setSearchTerm] = useState('') // state for searching value
+  const journeys = useSelector(({ journeys }) => journeys) // get the journeys data from the store
 
   useEffect(() => {
-    dispatch(initializeJourneys(currentPage))
-  }, [dispatch, currentPage])
+    const delay = setTimeout(() => {
+      // delay for searching
+      dispatch(
+        // dispatch values to the reducer
+        initializeJourneys(currentPage, sortField, sortOrder, searchTerm)
+      )
+    }, 1000)
+    return () => clearTimeout(delay) // clear the timeout
+  }, [dispatch, currentPage, sortField, sortOrder, searchTerm])
 
-  const handlePageChange = (e, value) => {
+  const handlePageChange = (event, value) => {
+    // handle page change and set the value
+    event.preventDefault()
     setCurrentPage(value)
+  }
+
+  // hande sorting and set the value
+  const handleSort = (value) => {
+    if ((sortField === '' && sortOrder === '') || sortOrder === 'desc') {
+      setSortField(value)
+      setSortOrder('asc')
+    } else {
+      setSortField(value)
+      setSortOrder('desc')
+    }
+  }
+
+  // handle sort clear
+  const handleClick = () => {
+    setSortField('')
+    setSortOrder('')
+  }
+
+  if (searchTerm) {
+    // if the search term exist show the details of the searched journeys
+    return (
+      <div>
+        <h2>Helsinki city bike journeys</h2>
+        <Box>
+          <TextField
+            type="search"
+            label="Search departure station by name"
+            value={searchTerm}
+            onChange={({ target }) => setSearchTerm(target.value)}
+            sx={{ width: 600 }}
+          />
+        </Box>
+        <SearchedJourneysData
+          journeys={journeys}
+          handleSort={handleSort}
+          handlePageChange={handlePageChange}
+        />
+      </div>
+    )
   }
 
   return (
     <div>
       <h2>Helsinki city bike journeys</h2>
-      {journeys.data ? (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow sx={{ boxShadow: 2 }}>
-                <TableCell sx={{ fontWeight: 'bold' }}>
-                  Departure station id
-                </TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Departure station name
-                </TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Return station name
-                </TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Covered distance (km)
-                </TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Duration (min)
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {journeys.data.map((journey) => (
-                <TableRow key={journey._id} sx={{ boxShadow: 2 }}>
-                  <TableCell>{journey.Departure_station_id}</TableCell>
-                  <TableCell align="right">
-                    {journey.Departure_station_name}
-                  </TableCell>
-                  <TableCell align="right">
-                    {journey.Return_station_name}
-                  </TableCell>
-                  <TableCell align="right">
-                    {(journey.Covered_distance_m / 1000).toFixed(2)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {(journey.Duration_sec / 60).toFixed()}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <CircularProgress />
-        </Box>
-      )}
-      <Stack spacing={3} sx={{ paddingTop: 2, paddingBottom: 2 }}>
-        <Pagination
-          sx={{ display: 'flex', justifyContent: 'center' }}
-          count={journeys.totalPages}
-          onChange={handlePageChange}
+      <Box>
+        <TextField
+          type="search"
+          label="Search departure station by name"
+          value={searchTerm}
+          onChange={({ target }) => setSearchTerm(target.value)}
+          sx={{ width: 600 }}
         />
-      </Stack>
+      </Box>
+      <Box style={{ paddingTop: 5 }}>
+        <p>clear sorting before searching</p>
+        <Button variant="contained" onClick={handleClick}>
+          clear sorting
+        </Button>
+      </Box>
+      <JourneysData
+        journeys={journeys}
+        sortOrder={sortOrder}
+        sortField={sortField}
+        handlePageChange={handlePageChange}
+        handleSort={handleSort}
+      />
     </div>
   )
 }
