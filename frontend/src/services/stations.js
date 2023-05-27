55934b44a041dd3c80e51868961619c61c32c8ac
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-27 22:53:22 +0300

    added comments

--- frontend/src/services/stations.js
+++ frontend/src/services/stations.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 const baseUrl = '/api/stations'
 
+//axios fetch the data from backend
 const getAll = async (currentPage) => {
   const request = await axios.get(`${baseUrl}?page=${currentPage}`)
   return request.data
 }
+//fetch the specific station data from backend
 const getStation = async (id) => {
   const request = await axios.get(`${baseUrl}/${id}`)
   return request.data
