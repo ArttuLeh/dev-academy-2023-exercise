868faa76aa2ba91c6eb5313230f6d1e64d021523
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-30 13:49:36 +0300

    data mapping moved to own component

--- frontend/src/components/StationsList.js
+++ frontend/src/components/StationsList.js
@@ -1,88 +1,69 @@
-import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import {
-  Paper,
-  TableContainer,
-  Table,
-  TableBody,
-  TableRow,
-  TableCell,
-  TableHead,
-  Stack,
-  Pagination,
-  Box,
-  CircularProgress,
-} from '@mui/material'
+import { Box, TextField } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { initializeStations } from '../reducers/stationsReducer'
+import SearchedStationsData from './SearchedStationData'
+import StationsData from './StationsData'
+import { toggleLoading } from '../reducers/loadingReducer'
 
 // component that lists all the stations
 // using material ui
 const StationList = () => {
   const dispatch = useDispatch()
   const [currentPage, setCurrentPage] = useState(1) // state for handling current page
+  const [searchTerm, setSearchTerm] = useState('') // state for searching value
   const stations = useSelector(({ stations }) => stations) // get the stations data from the store
+  const isLoading = useSelector(({ loading }) => loading) //loading state for handling loading image
 
   useEffect(() => {
-    // dispatch the currentpage value to the reducer whenever user change the page
-    dispatch(initializeStations(currentPage))
-  }, [dispatch, currentPage])
+    dispatch(toggleLoading(false))
+    const delay = setTimeout(() => {
+      // dispatch the currentpage value to the reducer whenever user change the page
+      dispatch(initializeStations(currentPage, searchTerm))
+    }, 1000)
+    return () => clearTimeout(delay)
+  }, [dispatch, currentPage, searchTerm])
 
   const handlePageChange = (e, value) => {
     // handle page change and set the value
     setCurrentPage(value)
   }
 
+  if (searchTerm) {
+    return (
+      <div>
+        <h2>Helsinki city bike stations</h2>
+        <Box>
+          <TextField
+            id="search"
+            type="search"
+            label="Search station by name"
+            value={searchTerm}
+            onChange={({ target }) => setSearchTerm(target.value)}
+            sx={{ width: 600 }}
+          />
+        </Box>
+        <SearchedStationsData
+          stations={stations}
+          handlePageChange={handlePageChange}
+        />
+      </div>
+    )
+  }
   return (
     <div>
       <h2>Helsinki city bike stations</h2>
-      {stations.data ? (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow sx={{ boxShadow: 2 }}>
-                <TableCell sx={{ fontWeight: 'bold' }}>Station name</TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Address
-                </TableCell>
-                <TableCell align="right" sx={{ fontWeight: 'bold' }}>
-                  Station ID
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {stations.data.map((station) => (
-                <TableRow key={station._id} sx={{ boxShadow: 2 }}>
-                  <TableCell>
-                    <Link to={`/stations/${station._id}`}>{station.Nimi}</Link>
-                  </TableCell>
-                  <TableCell align="right">
-                    {station.Osoite} {station.Kaupunki}
-                  </TableCell>
-                  <TableCell align="right">{station.ID}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <CircularProgress />
-        </Box>
-      )}
-      <Stack
-        spacing={3}
-        sx={{
-          paddingTop: 2,
-          paddingBottom: 2,
-        }}
-      >
-        <Pagination
-          sx={{ display: 'flex', justifyContent: 'center' }}
-          count={stations.totalPages}
-          onChange={handlePageChange}
+      <Box>
+        <TextField
+          id="search"
+          type="search"
+          label="Search station by name"
+          value={searchTerm}
+          onChange={({ target }) => setSearchTerm(target.value)}
+          sx={{ width: 600 }}
         />
-      </Stack>
+      </Box>
+      <StationsData stations={stations} handlePageChange={handlePageChange} />
     </div>
   )
 }
