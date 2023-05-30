e04291c731702eedce24bf1d43e670f4772a78b7
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-05-30 13:51:17 +0300

    try catch removed

--- frontend/src/reducers/stationReducer.js
+++ frontend/src/reducers/stationReducer.js
@@ -16,15 +16,11 @@ const stationSlice = createSlice({
 // dispatch the data to store
 export const getStation = (id) => {
   return async (dispatch) => {
-    try {
-      dispatch(toggleLoading(false))
-      // call axios
-      const data = await stationsService.getStation(id)
-      dispatch(setStation(data))
-      dispatch(toggleLoading(true))
-    } catch (error) {
-      console.error(error.message)
-    }
+    // call axios
+    const data = await stationsService.getStation(id)
+    dispatch(toggleLoading(false))
+    dispatch(setStation(data))
+    dispatch(toggleLoading(true))
   }
 }
 export const { setStation } = stationSlice.actions
